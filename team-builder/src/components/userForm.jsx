7b8ccc2c22307ed9e1580fa9bd4b166b514cc064
ThomasBettencourt <thomasbetten.cs@gmail.com>
2020-03-19 18:33:01 -0500

    made progress

--- team-builder/src/components/userForm.jsx
+++ team-builder/src/components/userForm.jsx
@@ -18,11 +18,43 @@ return (
     <Form>
         <p> UserName </p>
         <Field type="text" name="user" placeholder="username" />
+        {touched.user && errors.user && <p className="error"> {errors.user} </p> }
 
+        <p> E-Mail </p>
+        <Field type="text" name="email" placeholder="Enter E-Mail" />
+        {touched.email && errors.email && (
+            <p className="error">{errors.email}</p>
+        )}
 
+        <p>Password </p>
+        <Field type="password" name="password" placeholder="Enter Password" />
+        {touched.password && errors.password && (
+          <p className="error">{errors.password}</p>
+        )}
 
+        <Field className="dropdown" component="select" name="options">
+          <option>Please Choose an Option</option>
+          <option value="UI-Developer">UI Developer</option>
+          <option value="Front-End">Front End Developer</option>
+          <option value="Back-End">Back End Developer</option>
+          <option value="Data-Science">Data Science</option>
+        </Field> 
+        {touched.options && errors.options && (
+          <p className="error">{errors.options}</p>
+        )}
 
-    </Form>    
+        <label> 
+            {" "}
+            <p> Terms and Conditions</p>
+            <Field type="checkbox" name="terms" checked={values.terms} />
+        </label>        
+        {touched.terms && errors.terms && (
+            <p className="error"> {errors.terms} </p>
+        )}
+
+        <button type="submit"> Submit </button>
+    </Form>   
+    {userName.map(memberId)} 
     
     
     </div>
